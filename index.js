af879a820fabc2abd02a23df6e65c47076e15295
James A Cox <jacox@jacox.net>
2021-04-29 21:04:21 -0500

    third release candidate

--- index.js
+++ index.js
@@ -1,28 +1,70 @@
+// implement express server for REST api demo assignment 
+
 const express = require('express');
-const users = require('./users.json');
-
 const app = express();
+const fs = require('fs');
 
-app.get('/', function(req, res) {
-    res.send('Hello World');
+// read in posts.jason
+const posts = require('./posts.json');
+
+// allow req.body to use json format
+app.use(express.json());
+
+
+// add a new post. New post is in the body of the post request.
+// add to the database.   
+// return success message or error if we can't write the file
+app.post('/posts', (req, res) => {
+    // push new post at end of array
+    posts.push(req.body.newPost);
+    // write JSON to the file
+    let stringData = JSON.stringify(posts, null, 2);
+    fs.writeFile('posts.json', stringData, (err) => {
+        if (err) return res.status(500).json({message: err});
+    });
+    return res.status(200).json({message: 'new post added'});
 });
 
-app.get('/books', (req, res) => {
-    res.send('there are 4 books in store');
+// get all posts in response to get request to '/posts'
+app.get('/posts', (req, res) => {
+    return res.status(200).send(posts);
 });
 
-app.post('/', (req, res) => {
-    res.send('this is a post request'); 
+
+// get a post with a specific id specified in url paremeter
+app.get('/posts/:id', (req, res) => {
+    let id = req.params.id;
+    let foundpost = posts.find((testpost) => {
+        return testpost.id == id;
+    });
+    if (!foundpost) return res.status(404).json({message: 'post not found'});
+    else {
+        return res.status(200).json({user: foundpost});
+    }
 });
 
-app.get('/users', (req, res) => {
-    return res.json({ users });
+// update a post by id with the provided new post
+app.put('/posts/:id', (req, res) => {
+    // retrieve id in url
+    let id = req.params.id;
+    // find index of that id in posts array
+    let foundpostindex = posts.findIndex((testpost) => {
+        return testpost.id == id;
+    });
+    // if not error, replace indexed record with new post
+    // and write out file again
+    if (foundpostindex == -1) return res.status(404).json({message: 'post not found'});
+    else {
+        posts[foundpostindex] = req.body.newPost;
+        let stringData = JSON.stringify(posts, null, 2);
+        fs.writeFile('posts.json', stringData, (err) => {
+            if (err) return res.status(500).json({message: err});
+        });
+        return res.status(200).json({user: posts[foundpostindex]});
+    }
 });
 
-app.post('/users', (req, resp) => {
-    
-})
-
+// start server up
 app.listen(3000, function () {
     console.log('Server is up and running');
 });
